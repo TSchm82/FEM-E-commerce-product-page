780ca0705d351a71f38b0b1e114d98c1985afc2a
Tobias Schmidt <tobias.schmidt@shapefield.de>
2022-10-06 19:26:01 +0200

    added node config

--- src/components/item/item.component.ts
+++ src/components/item/item.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+const fs = require('../../assets/images');
+const images = fs.readdirSync('../');
+
 @Component({
   selector: 'component-item',
   templateUrl: './item.component.html'
 })
 export class ItemComponent implements OnInit {
 
+  public imgPath = '../../assets/images/'
+
+  public imgNames = [
+    'icon-previous.svg',
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(images)
   }
 
 }
